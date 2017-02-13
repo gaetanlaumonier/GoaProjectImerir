508f58ac5436dde0126801230a58da34044a234d
Student <student@MacBook-Air-de-Service.local>
2017-02-13 18:25:03 +0100

    Ajout fenêtre arcade rangement

---
+++ ProjetImerirApp/RangementViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RangementViewController.swift
+//  ProjetImerirApp
+//
+//  Created by Student on 12/02/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class RangementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
