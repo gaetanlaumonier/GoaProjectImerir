d151e184465dd7a245a5e5f995fabd27d0707dfd
gaetanlaumonier <student@MacBook-Air-de-Service-4.local>
2017-02-14 10:55:22 +0100

    Lala commit

---
+++ ProjetImerirApp/ClasseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ClasseTableViewCell.swift
+//  ProjetImerirApp
+//
+//  Created by Student on 13/02/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ClasseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleClasseLabel: DesignableLabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
